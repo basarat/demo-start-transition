46b10e520c463b9d5069d22c3231fc752bf1cff9
basarat <basaratali@gmail.com>
2022-05-23 09:16:31 +1000

    with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,31 @@
+import { useState } from 'react';
+import { searchItems, List, randomChars } from './List';
+
 function App() {
+  const [value, setValue] = useState('');
+  const [items, setItems] = useState(searchItems(value));
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const value = e.target.value;
+    setValue(value);
+    setItems(searchItems(value));
+  };
+
+  const handleRandom = () => {
+    const value = randomChars();
+    setValue(value);
+    setItems(searchItems(value));
+  }
+
   return (
-    null
+    <div>
+      <div>
+        <input value={value} onChange={handleChange} />
+        <button onClick={handleRandom}>Random</button>
+      </div>
+
+      <List items={items} />
+    </div>
   );
 }
 
