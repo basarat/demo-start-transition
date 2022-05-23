135a6d3670b2490d3d35712d6017e080ed2418a7
basarat <basaratali@gmail.com>
2022-05-23 13:40:49 +1000

    wrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import { searchItems, List, randomChars } from './List';
 
 function App() {
+  const [value, setValue] = useState<string>('');
+  const [items, setItems] = useState<string[]>([]);
+  const [isPending, startTransition] = useTransition();
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const value = e.target.value;
+    setValue(value);
+    startTransition(() => {
+      setItems(searchItems(value));
+    });
+  };
+
+  const handleRandom = () => {
+    const value = randomChars();
+    setValue(value);
+    setItems(searchItems(value));
+  };
+
   return (
-    null
+    <div>
+      <div style={{ opacity: isPending ? .5 : 1 }}>
+        <input value={value} onChange={handleChange} />
+        <button onClick={handleRandom}>Random</button>
+      </div>
+      <List items={items} />
+    </div>
   );
 }
 
